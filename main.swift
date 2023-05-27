f079e6b9c3bce8bf785977b004ed7ba9f4dc9a9f
Jadejaparikshitsinh <125565997+Jadejaparikshitsinh@users.noreply.github.com>
2023-05-27 11:21:02 +0530

    Add files via upload

--- main.swift
+++ main.swift
@@ -1,54 +1,58 @@
 //
 //  main.swift
-//  array
+//  condition
 //
-//  Created by JADEJA PARIKSHITSINH on 23/05/23.
+//  Created by JADEJA PARIKSHITSINH on 27/05/23.
 //
 
 import Foundation
 
-/*var array = ["rajkot","junagadh","ahmdavad"]
-//print(array)
-array.append("baroda")
-array.append(contentsOf: ["juhu","beach"])
-array.insert("austrilia", at: 0)
-array.insert(contentsOf: ["newzland","africa","abhganistan","baluchistan","naizeriya"], at: 0)
-//array.removeAll()
-//array.removeFirst()
-//array.removeFirst(3)
-//array.removeLast()
-//array.removeLast(3)
-//array.remove(at: 1)
-//array.removeFirst(3)
-//array.sort()
-//array.reverse()
-print(array.count)
+var a:Int,b:Int,c:Int
+
+print("enter number of a: ")
+a = Int(readLine()!)!
+print("enter number of b:")
+b = Int(readLine()!)!
+print("enter number of c:")
+c = Int(readLine()!)!
+//print("sum is", a+b+c)
 
 
-print(array)
-
-for i in 0..<array.count
+ if a<=35&&b<=35&&c<=35
 {
-   // print(i)
-    //print(array[i])
-    print("\(i)=\(array[i])")
-    
-}*/
-
-var alpha:Set=["A","B","C","D","E","f","g"]
-alpha.reversed()
-//alpha.remove("C")
-//alpha.removeAll()
-//alpha.removeFirst()
-print(alpha.count)
-print(alpha)
-	
-for i in 0..<alpha.count
-        
-{
-    print(i)
-}
-var newest:Set=["P","r","Q","s"]
-//let x = alpha.union(newest)
-let x = alpha.intersection(newest)
-print(x)
+     var total = a+b+c
+     print("total:\(total)")
+     if a<b
+            
+     {
+         print("a is min")
+     }
+     else
+     {
+         print("a is max")
+     }
+     if   b<c
+     {
+         print("b is min")
+     }
+     else
+     {
+         print("b is max")
+     }
+     if c<a
+     {
+         print("c is min")
+     }
+     else
+     {
+         print("c is max")
+     }
+     if a==b
+     {
+         print("value are same")
+     }
+     if b==c
+     {
+         print("value are same")
+     }
+ }
